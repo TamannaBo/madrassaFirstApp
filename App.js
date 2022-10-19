45972564ed843fccfc13545c1907dbbf46879530
TamannaBo <116170159+TamannaBo@users.noreply.github.com>
2022-10-19 13:10:52 +0100

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,12 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
 	return (
 		<View style={styles.container}>
-			<Text>mussa</Text>
+			<Text>Mussa Mendes</Text>
+                        <Text>Tamanna Sayed</Text>
+                        <Text> Tamanna Sayed<Text>
+<Text>My name is Tamanna and i am 17 years old. 
+I am a student and i like to play 
+video games in my free time<Text>
 			<StatusBar style="auto" />
 		</View>
 	);
